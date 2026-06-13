053a1ac8849748339af6bbadd5814969add6d436
Kram <marlon.andres66@gmail.com>
2026-06-13 03:15:30 -0500

    Formu

--- proyecto.js
+++ proyecto.js
@@ -38,3 +38,21 @@ regis.addEventListener('click', function() {
         alert('Registro exitoso');
     }
 });
+
+//formulario
+
+const enviar = document.getElementById('enviar');
+
+enviar.addEventListener('click', function() {
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const cedula = document.getElementById('cedula').value;
+    const celular = document.getElementById('celular').value;
+    const email = document.getElementById('email').value;
+
+    if (nombre === '' || apellido === '' || cedula === '' || celular === '' || email === '' ) {
+             alert('Por favor, complete todos los campos.');
+    } else {
+        alert('Registro exitoso');
+    }
+});
